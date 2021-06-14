449cc9767e306397e1c74882abd75dfc2aa6739d
avavion <avavionmvm@gmail.com>
2021-06-15 02:32:01 +0300

    Готовая секция header

--- scripts/index.js
+++ scripts/index.js
@@ -5,4 +5,6 @@ document.addEventListener("DOMContentLoaded", function () {
     // Навигационное меню
     const navbar = header.querySelector("#navbar");
     
+
+    localStorage.getItem("whishlist")
 });
